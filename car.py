90e0708950e675941f8da6ebd2552ccf69fe1a7a
JackBlair87 <60362058+JackBlair87@users.noreply.github.com>
2020-08-29 00:22:42 -0400

    Driving Car Game works

--- car.py
+++ car.py
@@ -1,20 +1,82 @@
-import pygame
+import pygame as pg
+import math
+import pygame as pg
+import collections
+from math import sin, radians, degrees, copysign
+from pygame.math import Vector2
+
+
+RACECAR = pg.image.load('./assets/OrangeCar.png')
 
 class Car:
-    def __init__(self):
-        self.location = (x, y)
-        self.speed_multiplier = 1
-        self.heading = 90
-        self.acceleration = 0
-        self.environmental_deceleration = -10 * speed_multiplier
-        self.velocity = 0
-    
-    def change_acceleration(self, change_amount):
-        self.acceleration += change_amount
-        self.velocity -= self.environmental_deceleration
-        self.velocity += 
+    def __init__(self, image, xLoc, yLoc, max_acceleration = 0.0001, max_steering = 45):
+        #Graphics
+        pg.sprite.Sprite.__init__(self)
+        self.image = image.convert_alpha() #This gets the clear image backgrounds
+        self.image = pg.transform.scale(self.image, (int(800 / 4), int(397 / 4)))
+        
+        #Physics
+        self.position = Vector2(xLoc, yLoc)
+        self.velocity = Vector2(0.0, 0.0)
+        self.acceleration = 0.0
+        self.angle = 0
+        self.steering = 0.0
+        
+        #Characteristics
+        #self.length = 100.0
+        #self.width = 50.0
+        self.length = self.image.get_size()[0]
+        self.width = self.image.get_size()[1]
+        self.max_acceleration = max_acceleration #Default is 0.001
+        self.max_steering = max_steering #Default is 1
+        self.max_velocity = 1
+        self.brake_deceleration = 5.0
+        self.free_deceleration = 0.5
 
-    def update_location():
-        """
-        should be called once per display tick
-        """
+    def update(self, dt):
+        self.velocity += (self.acceleration * dt, 0)
+        self.velocity.x = max(-self.max_velocity, min(self.velocity.x, self.max_velocity))
+        angular_velocity = 0
+        if self.steering != 0.0:
+            angular_velocity = self.velocity.x / (self.length / sin(radians(self.steering)))
+        
+        self.position += self.velocity.rotate(-self.angle) * dt
+        self.angle += degrees(angular_velocity) * dt
+        
+    def getInput(self, pressed, dt):
+        if pressed[pg.K_UP]:
+                if self.velocity.x < 0:
+                    self.acceleration = self.brake_deceleration
+                else:
+                    self.acceleration += 1 * dt
+        elif pressed[pg.K_DOWN]:
+            if self.velocity.x > 0:
+                self.acceleration = -self.brake_deceleration
+            else:
+                self.acceleration -= 1 * dt
+        elif pressed[pg.K_SPACE]:
+            if abs(self.velocity.x) > dt * self.brake_deceleration:
+                self.acceleration = -copysign(self.brake_deceleration, self.velocity.x)
+            else:
+                self.acceleration = -self.velocity.x / dt
+        else:
+            if abs(self.velocity.x) > dt * self.free_deceleration:
+                self.acceleration = -copysign(self.free_deceleration, self.velocity.x)
+            else:
+                if dt != 0:
+                    self.acceleration = -self.velocity.x / dt
+        self.acceleration = max(-self.max_acceleration, min(self.acceleration, self.max_acceleration))
+
+        if pressed[pg.K_RIGHT]:
+            self.steering -= 30 * dt
+        elif pressed[pg.K_LEFT]:
+            self.steering += 30 * dt
+        else:
+            self.steering = 0
+        self.steering = max(-self.max_steering, min(self.steering, self.max_steering))
+        
+    def draw(self, screen, middleX, middleY):
+        rotated = pg.transform.rotate(self.image, self.angle)
+        rect = rotated.get_rect()
+        screen.blit(rotated, (middleX - rect.width / 2, middleY - rect.height / 2))
+        #screen.blit(rotated, (self.position.x - rect.width / 2, self.position.y - rect.height / 2))
--- display.py
+++ display.py
@@ -1,13 +1,22 @@
 import pygame as pg
 import numpy as np
+from car import Car
 
 SCREEN_WIDTH = 1440
 SCREEN_HEIGHT = 800
 
 BLACK = (0, 0, 0)
 GRASS_GREEN = (106, 140, 76)
-
 RACECAR = pg.image.load('./assets/OrangeCar.png')
+
+VERT_TRACK = pg.image.load('./assets/VerticalTrack.jpeg')
+HOR_TRACK = pg.image.load('./assets/HorizontalTrack.jpeg')
+LEFT_TURN = pg.image.load('./assets/LeftTurn.jpeg')
+RIGHT_TURN = pg.image.load('./assets/RightTurn.jpeg')
+FINISH_LINE = pg.image.load('./assets/FinishLine.jpeg')
+
+TRACK_1 = pg.image.load('./assets/Track1.jpeg')
+TRACK_1 = pg.transform.scale(TRACK_1, (16000, 8000))
 
 def quitGame(): #Quits Pygame and Python
     pg.quit()
@@ -21,33 +30,28 @@ def backgroundInputCheck(eventList): #Constantly checks for quits and enters
                 if event.key == pg.K_ESCAPE:
                     quitGame()
                     
-def runGame():
+def main():
+    playerCar = Car(RACECAR, 6800, 2300)
     
-    print("HI")
     while True:
         deltaTime = clock.get_time()
         backgroundInputCheck(pg.event.get())
         
-        print("Pi")
+        playerCar.getInput(pg.key.get_pressed(), deltaTime)
+        playerCar.update(deltaTime)
+        
         #tick racecar
         screen.fill(GRASS_GREEN)
-        
-        #draw car
-        #screen.blit(RACECAR, (0, 0))
-        
+        screen.blit(TRACK_1, (-playerCar.position.x, -playerCar.position.y))
+        playerCar.draw(screen, SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+         
         clock.tick(60)
         pg.display.flip()
     
-def draw_track():
-    gameDisplay = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
-
-
-
-
 if __name__ == "__main__":
     pg.init()
     screen = pg.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
     pg.display.set_caption("PBS Prix")
     clock = pg.time.Clock()
-    runGame()
+    main()
